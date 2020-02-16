84bcaa863c9258e3fbaa2fe63a89631eb2f02c84
Dariusz Marciniak <darekmarciniak69@gmail.com>
2020-02-16 16:58:46 +0100

    final version

--- StarWarsCharacters/resources/views/sites/ApiList.blade.php
+++ StarWarsCharacters/resources/views/sites/ApiList.blade.php
@@ -4,17 +4,22 @@
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
 <body>
+<h2 class="text-center"> Star Wars Characters</h2>
+<div class="container">
 <ol>
 @foreach($star_wars as $element)
-        <ul class="list-group">
+        <ul class="list-group ">
             <li class="list-group-item list-group-item-info">Name : {{  $element['name'] }}</li>
-            <li class="list-group-item">Height :  {{  $element['height']  }}</li>
+            <li class="list-group-item ">Height :  {{  $element['height']  }}</li>
             <li class="list-group-item"> Mass  : {{  $element['mass']  }}</li>
+            <li class="list-group-item"> Eyes color  : {{  $element['eye_color']  }}</li>
             <li class="list-group-item"> Birth Year  : {{  $element['birth_year']  }}</li>
             <li class="list-group-item"> Gender  : {{  $element['gender']  }}</li>
+
         </ul>
         </li>
 @endforeach
 </ol>
+</div>
 </body>
 </html>
--- StarWarsCharacters/storage/framework/views/2afec4c2f324412148fcae7a929d48b7f4755e53.php
+++ StarWarsCharacters/storage/framework/views/2afec4c2f324412148fcae7a929d48b7f4755e53.php
@@ -4,18 +4,23 @@
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
 <body>
+<h2 class="text-center"> Star Wars Characters</h2>
+<div class="container">
 <ol>
 <?php $__currentLoopData = $star_wars; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $element): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-        <ul class="list-group">
+        <ul class="list-group ">
             <li class="list-group-item list-group-item-info">Name : <?php echo e($element['name']); ?></li>
-            <li class="list-group-item">Height :  <?php echo e($element['height']); ?></li>
+            <li class="list-group-item ">Height :  <?php echo e($element['height']); ?></li>
             <li class="list-group-item"> Mass  : <?php echo e($element['mass']); ?></li>
+            <li class="list-group-item"> Eyes color  : <?php echo e($element['eye_color']); ?></li>
             <li class="list-group-item"> Birth Year  : <?php echo e($element['birth_year']); ?></li>
             <li class="list-group-item"> Gender  : <?php echo e($element['gender']); ?></li>
+
         </ul>
         </li>
 <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
 </ol>
+</div>
 </body>
 </html>
 <?php /**PATH /Applications/MAMP/htdocs/starWars/StarWarsCharacters/resources/views//sites/ApiList.blade.php ENDPATH**/ ?>
