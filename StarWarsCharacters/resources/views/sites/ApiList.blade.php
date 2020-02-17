5d89ae9e91ae52c1836f343493e00888bf6f193f
Dariusz Marciniak <darekmarciniak69@gmail.com>
2020-02-17 13:37:03 +0100

    layout -edit

--- StarWarsCharacters/resources/views/sites/ApiList.blade.php
+++ StarWarsCharacters/resources/views/sites/ApiList.blade.php
@@ -1,20 +1,20 @@
 <html>
-
 <head>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="/css/index.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-<h2 class="text-center"> Star Wars Characters</h2>
+
 <div class="container">
 <ol>
 @foreach($star_wars as $element)
         <ul class="list-group ">
-            <li class="list-group-item list-group-item-info">Name : {{  $element['name'] }}</li>
-            <li class="list-group-item ">Height :  {{  $element['height']  }}</li>
-            <li class="list-group-item"> Mass  : {{  $element['mass']  }}</li>
-            <li class="list-group-item"> Eyes color  : {{  $element['eye_color']  }}</li>
-            <li class="list-group-item"> Birth Year  : {{  $element['birth_year']  }}</li>
-            <li class="list-group-item"> Gender  : {{  $element['gender']  }}</li>
+            <li class="list-group-item" id="name"> {{  $element['name'] }}</li>
+            <li class="list-group-item" id="height"> Height :  {{  $element['height']  }}</li>
+            <li class="list-group-item "id="mass"> Mass  : {{  $element['mass']  }}</li>
+            <li class="list-group-item" id="eyesColor"> Eyes color  : {{  $element['eye_color']  }}</li>
+            <li class="list-group-item" id="year"> Birth Year  : {{  $element['birth_year']  }}</li>
+            <li class="list-group-item" id="gender"> Gender  : {{  $element['gender']  }}</li>
 
         </ul>
         </li>
--- StarWarsCharacters/storage/framework/views/2afec4c2f324412148fcae7a929d48b7f4755e53.php
+++ StarWarsCharacters/storage/framework/views/2afec4c2f324412148fcae7a929d48b7f4755e53.php
@@ -1,20 +1,20 @@
 <html>
-
 <head>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="/css/index.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-<h2 class="text-center"> Star Wars Characters</h2>
+
 <div class="container">
 <ol>
 <?php $__currentLoopData = $star_wars; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $element): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
         <ul class="list-group ">
-            <li class="list-group-item list-group-item-info">Name : <?php echo e($element['name']); ?></li>
-            <li class="list-group-item ">Height :  <?php echo e($element['height']); ?></li>
-            <li class="list-group-item"> Mass  : <?php echo e($element['mass']); ?></li>
-            <li class="list-group-item"> Eyes color  : <?php echo e($element['eye_color']); ?></li>
-            <li class="list-group-item"> Birth Year  : <?php echo e($element['birth_year']); ?></li>
-            <li class="list-group-item"> Gender  : <?php echo e($element['gender']); ?></li>
+            <li class="list-group-item" id="name"> <?php echo e($element['name']); ?></li>
+            <li class="list-group-item" id="height"> Height :  <?php echo e($element['height']); ?></li>
+            <li class="list-group-item "id="mass"> Mass  : <?php echo e($element['mass']); ?></li>
+            <li class="list-group-item" id="eyesColor"> Eyes color  : <?php echo e($element['eye_color']); ?></li>
+            <li class="list-group-item" id="year"> Birth Year  : <?php echo e($element['birth_year']); ?></li>
+            <li class="list-group-item" id="gender"> Gender  : <?php echo e($element['gender']); ?></li>
 
         </ul>
         </li>
