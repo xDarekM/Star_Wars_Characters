982aa11f6be750c9e5fbc626d9933371b711967f
Dariusz Marciniak <darekmarciniak69@gmail.com>
2020-04-15 12:25:51 +0200

    input-edit

---
+++ StarWarsCharacters/app/Http/Controllers/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+
+class UserController
+{
+
+
+    public function store(Request $request){
+        $name = $request->input('name');
+    }
+
+
+
+}
